63736850964ce11caeb29e11f6c98ff6b54949bc
suraj-subin <surajsubin5@gmail.com>
2023-02-18 21:54:56 +0545

    Modern-UI-UX-Website

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './article.css';
+
+const Article = () => {
+  return (
+    <>Article</>
+  )
+}
+
+export default Article;
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './brand.css';
+
+const Brand = () => {
+  return (
+    <>Brand</>
+  )
+}
+
+export default Brand;   
